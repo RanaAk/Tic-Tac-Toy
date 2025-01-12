ee77b2a2de62f15985ac9da023477a96d5d9e386
RanaAk <mahediAkbar02@gmail.com>
2025-01-12 14:01:52 +0600

    Initial Commit

---
+++ Tic Tac Toy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic Tac Toy
+//
+//  Created by RANA  on 12/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tic Tac Toy/Tic_Tac_ToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToyApp.swift
+//  Tic Tac Toy
+//
+//  Created by RANA  on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
